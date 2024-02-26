ae310bd6f95a75628db4e1503d6cc86ae86a0587
SonaliFlorenceG <119723770+SonaliFlorenceG@users.noreply.github.com>
2024-02-26 10:52:16 +0530

    Add files via upload

---
+++ maps.cpp
@@ -0,0 +1,71 @@
+//doesn't work in dev :
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	map<string,int>dsp;
+//	dsp["first"]=10;
+//	for (auto it:dsp)
+//	{
+//		cout<<it.first<<" "<<it.second<<endl;
+//	}
+//}
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	map<string,int>dsp;
+//	dsp["one"]=10;
+//	dsp["two"]=20;
+//	dsp["three"]=30;
+//	
+//	for (auto it:dsp)
+//	{
+//		cout<<it.first<<" "<<it.second<<endl;
+//	}
+//}
+
+//Ordered map:
+	
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	map<int,int>dsp;
+//	vector<int>v={1,1,1,2,3,4,3,4,5,6,7,7};
+//	for (int i=0; i<v.size();i++)
+//	{
+//		dsp[v[i]]+=1;
+//	}
+//	for (auto it:dsp)
+//	{
+//		cout<<it.first<<" "<<it.second<<endl;
+//	}
+//}
+
+//Unordered map:
+	
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	unordered_map<int,int>dsp;
+//	vector<int>v={1,1,1,2,3,4,3,4,5,6,7,7};
+//	for (int i=0; i<v.size();i++)
+//	{
+//		dsp[v[i]]+=1;
+//	}
+//	for (auto it:dsp)
+//	{
+//		cout<<it.first<<" "<<it.second<<endl;
+//	}
+//}
+
+
+
+
+
+
+
